e69d601d7f7c9f39df0aba81ab27e6dbf5f734d5
Gerardp <agerardo1397@gmail.com>
2020-08-14 11:06:10 -0500

    timing visual effect fix

--- js/animation.js
+++ js/animation.js
@@ -2,22 +2,22 @@ anime({
     targets: ['.red_section_wrapper', '.menu .right'],
     opacity: [0, 1],
     easing: 'easeInOutSine',
-    delay: 900,
+    delay: 1500,
     duration: 1000
 });
 
 anime({
-    targets: ['.menu .left'],
+    targets: ['.menu .left', '.menu'],
     opacity: [0, 1],
     easing: 'easeInOutSine',
     delay: 300,
-    duration: 700
+    duration: 500
 });
 anime({
     targets: ['.line-div'],
     opacity: [0, 1],
     easing: 'easeInOutSine',
-    delay: 400,
+    delay: 700,
     duration: 1300
 });
 
@@ -25,14 +25,14 @@ anime({
     targets: '.der',
     opacity: [0, 1],
     easing: 'easeInOutSine',
-    delay: 1000,
-    duration: 1000
+    delay: 1200,
+    duration: 800
 });
 
 anime({
     targets: '.pop-up',
     opacity: [0, 1],
     easing: 'easeInOutSine',
-    delay: 2000,
+    delay: 2300,
     duration: 1000
 });
