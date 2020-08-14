6ce359401239f84ea35e3891b2988541995d44b7
Gerardp <agerardo1397@gmail.com>
2020-08-14 10:58:01 -0500

    efectos visuales index

---
+++ js/animation.js
@@ -0,0 +1,38 @@
+anime({
+    targets: ['.red_section_wrapper', '.menu .right'],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    delay: 900,
+    duration: 1000
+});
+
+anime({
+    targets: ['.menu .left'],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    delay: 300,
+    duration: 700
+});
+anime({
+    targets: ['.line-div'],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    delay: 400,
+    duration: 1300
+});
+
+anime({
+    targets: '.der',
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    delay: 1000,
+    duration: 1000
+});
+
+anime({
+    targets: '.pop-up',
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    delay: 2000,
+    duration: 1000
+});
